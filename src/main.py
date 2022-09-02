75c9396e89f48c96fc45bcbae7adba6abbab5996
Maxim Kolomeychenko <maxim@supervise.ly>
2022-09-02 12:49:55 +0300

    todo

--- src/main.py
+++ src/main.py
@@ -172,6 +172,10 @@ def process(api: sly.Api, task_id, context, state, app_logger):
     api.task.set_output_project(task_id, project.id, project.name)
 
 
+# TODO:
+# empty folder - icon with nothing
+
+
 def main():
     data = {}
     state = {}
